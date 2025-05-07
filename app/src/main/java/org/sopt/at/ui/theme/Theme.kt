d5eccc9632e20711b75fbcc28dec832347874fca
JAY <wndkdus102@naver.com>
2025-05-07 18:22:44 +0900

    [add]design system 추가

--- app/src/main/java/org/sopt/at/ui/theme/Theme.kt
+++ app/src/main/java/org/sopt/at/ui/theme/Theme.kt
@@ -9,50 +9,59 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+object ATSOPTANDROIDTheme{
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    val colors: TvingColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTvingColorsProvider.current
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+    val typography: TvingTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTvingTypographyProvider.current
+
+}
+
+@Composable
+fun ProvideATSOPTANDROIDColorAndTypography(
+    colors: TvingColors,
+    typography: TvingTypography,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalTvingColorsProvider provides colors,
+        content = content
+    )
+}
+
 
 @Composable
 fun ATSOPTANDROIDTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    ProvideATSOPTANDROIDColorAndTypography (
+        colors = defaultTvingColors,
+        typography = defaultTvingTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = false
+                }
+            }
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        MaterialTheme(
+            content = content
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
