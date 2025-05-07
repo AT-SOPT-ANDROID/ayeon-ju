b4e1bf62f08cd058c85e0a296c1abdde83194e16
JAY <wndkdus102@naver.com>
2025-05-08 05:46:03 +0900

    [add] userRepository 추가

---
+++ app/src/main/java/org/sopt/at/domain/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package org.sopt.at.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+
+    suspend fun saveUserInfo(id: String, password: String)
+    fun getUserId(): Flow<String>
+    fun getUserPassword(): Flow<String>
+}
